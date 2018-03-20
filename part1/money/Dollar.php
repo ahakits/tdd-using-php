457d783bf257f6ce6681ee14df4ae664ec6ae914
ahakits <ahakits1987@gmail.com>
2018-03-20 12:16:03 +0900

    Chapter9 green

--- part1/money/Dollar.php
+++ part1/money/Dollar.php
@@ -11,7 +11,7 @@ class Dollar extends Money
 
     public function currency(): string
     {
-        return "";
+        return "USD";
     }
 
 
--- part1/money/Franc.php
+++ part1/money/Franc.php
@@ -11,7 +11,7 @@ class Franc extends Money
 
     public function currency(): string
     {
-        return "";
+        return "CHF";
     }
 
 
