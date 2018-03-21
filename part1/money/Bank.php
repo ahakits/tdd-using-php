cb24dcce9dc4b416fe2a9e1e41b722a077be1ed9
ahakits <ahakits1987@gmail.com>
2018-03-21 23:20:40 +0900

    Chapter14 red

--- part1/money/Bank.php
+++ part1/money/Bank.php
@@ -4,12 +4,20 @@ namespace Part1\Money;
 
 class Bank
 {
+    private $rates = [];
+
     public function reduce(Expression $source, string $to)
     {
-        return $source->reduce($to);
+        return $source->reduce($this, $to);
     }
 
     public function addRate(string $from, string $to, int $rate): void
     {
+        $this->rates[(new Pair($from, $to))->hashCode()] = $rate;
+    }
+
+    public function rate(string $from, string $to): int
+    {
+        return $this->rates[(new Pair($from, $to))->hashCode()];
     }
 }
--- part1/money/Money.php
+++ part1/money/Money.php
@@ -39,9 +39,9 @@ class Money implements Expression
             && $this->currency() === $other->currency();
     }
 
-    public function reduce(string $to): Money
+    public function reduce(Bank $bank, string $to): Money
     {
-        $rate = ($this->currency === "CHF" && $to === "USD") ? 2 : 1;
+        $rate = $bank->rate($this->currency, $to);
         return new Money($this->amount / $rate, $to);
     }
 
