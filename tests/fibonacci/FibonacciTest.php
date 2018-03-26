ad4e1b8c414bac8f8f604bbac04b5e65e645cd54
ahakits <ahakits1987@gmail.com>
2018-03-26 17:16:08 +0900

    Fibonacci: red

--- tests/fibonacci/FibonacciTest.php
+++ tests/fibonacci/FibonacciTest.php
@@ -9,7 +9,9 @@ class FibonacciTest extends TestCase
 {
     public function testFibonacci()
     {
-        $this->assertEquals(0, Fibonacci::fib(0));
-        $this->assertEquals(1, Fibonacci::fib(1));
+        $cases = [[0, 0], [1, 1], [2, 1], [3, 2]];
+        for ($i = 0; $i < count($cases); $i++) {
+            $this->assertEquals($cases[$i][1], Fibonacci::fib($cases[$i][0]));
+        }
     }
 }
